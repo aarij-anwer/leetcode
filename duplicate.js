408deea8b1083f411fc08a99fdc05ff960101ffa
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-05-29 13:10:27 -0400

    add some tests

--- duplicate.js
+++ duplicate.js
@@ -6,3 +6,14 @@ const containsDuplicate = (nums) => {
     const uniqueValues = new Set(nums);
     return !(nums.length == uniqueValues.size);
 };
+
+let nums;
+
+nums = [1,2,3,1];
+console.log(nums, containsDuplicate(nums));
+
+nums = [1,2,3,4];
+console.log(nums, containsDuplicate(nums));
+
+nums = [1,1,1,3,3,4,3,2,4,2];
+console.log(nums, containsDuplicate(nums));

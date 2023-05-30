24eddfa2dcfcdc9326d2aba2af34b889783bf7f3
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-05-30 14:52:00 -0400

    init

---
+++ twosum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+const twoSum = (nums, target) => {
+
+};
+
+let nums; let target;
+
+nums = [2,7,11,15]; target = 9;
+console.log(nums, target, twoSum(nums,target));

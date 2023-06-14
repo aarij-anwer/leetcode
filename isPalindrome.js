4851a30aa5713e85e535a9ab0143283aec81628b
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-06-14 18:11:44 -0400

    isPalindrome

---
+++ isPalindrome.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = (s) => {
+  const str = s.replace(/[^A-Za-z0-9]/g,"").toLowerCase().split("");
+  const middleIndex = str.length % 2 === 0 ? str.length/2 : (str.length - 1)/2;
+
+  for (let i = 0; i < middleIndex ; i++) {
+      if (str[i] !== str[str.length-1-i]) {
+          return false;
+      }
+  }
+  return true;
+};
+
+let s;
+
+s = "A man, a plan, a canal: Panama";
+console.log(s);
+console.log("isPalindrome:", isPalindrome(s));
+
+s = "A man, a plan, a canal: Panama";
+console.log(s);
+console.log("isPalindrome:", isPalindrome(s));
+
+
